46cb92c05d1d05112eb9a9713c40c455f0c94602
SomgBird <dlarionov51@gmail.com>
2026-09-23 03:40:38 +0300

    default background color

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,21 @@
+import { useEffect } from "react";
 import ControlPanel from "./components/ControlPanel/ControlPanel";
 import Explorer from "./components/Explorer/Explorer";
 
 function App() {
+  useEffect(() => {
+    document.body.style.backgroundColor = "#008080";
+  });
+
   return (
-    <div style={{ display: "flex", gap: "1rem", alignItems: "center" }}>
+    <div
+      style={{
+        display: "flex",
+        gap: "1rem",
+        alignItems: "center",
+        backgroundColor: "#008080",
+      }}
+    >
       <ControlPanel />
       <Explorer />
     </div>
